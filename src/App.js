7d3b4f82188a106862b9f4dc5bf61df425200079
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-06 11:12:12 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import Title from "./components/Title";
 import Input from "./components/Input";
 import Lane from "./components/Lane";
+import { useTodo } from "./contexts/ActionContext";
 import "./styles.css";
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
-  const handleAddTodo = (todoInput) => {
-    setTodos([...todos, { id: Date.now(), content: todoInput, type: "todo" }]);
-  };
+  const [, { handleAddTodo }] = useTodo();
+
   return (
     <div className="container">
       <Title>Simple Kanban Board</Title>
       <Input onSubmit={handleAddTodo} />
-      <Lane todos={todos} setTodos={setTodos} />
+      <Lane />
     </div>
   );
 }
