d01dad228c50e1b06ab6947403eb8d8921b61ff2
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-06 11:14:57 +0700

    edit-index

--- src/components/Lane/index.js
+++ src/components/Lane/index.js
@@ -1,65 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Content from "./Content";
-import ActionContext from "../../contexts/ActionContext";
+import { useTodo } from "../../contexts/ActionContext";
 
-function Lane({ todos, setTodos }) {
-  // const [doings, setDoings] = useState([]);
-  // const [dones, setDones] = useState([]);
-  const handleTodoClick = (itemId) => {
-    const cloneTodods = [...todos]; //clone with reference changed
-    const itemIndex = cloneTodods.findIndex((todo) => todo.id === itemId);
-    if (cloneTodods[itemIndex]) {
-      cloneTodods[itemIndex].type = "todo";
-    }
-    setTodos(cloneTodods);
-  };
-  const handleDoClick = (itemId) => {
-    const cloneTodods = [...todos]; //clone with reference changed
-    const itemIndex = cloneTodods.findIndex((todo) => todo.id === itemId);
-    if (cloneTodods[itemIndex]) {
-      cloneTodods[itemIndex].type = "doing";
-    }
-    setTodos(cloneTodods);
-  };
-  const handleDoneClick = (itemId) => {
-    const cloneTodods = [...todos]; //clone with reference changed
-    const itemIndex = cloneTodods.findIndex((todo) => todo.id === itemId);
-    if (cloneTodods[itemIndex]) {
-      cloneTodods[itemIndex].type = "done";
-    }
-    setTodos(cloneTodods);
-  };
-
-  useEffect(() => {
-    setTodos(JSON.parse(window.localStorage.getItem("todos")));
-  }, [setTodos]);
-  useEffect(() => {
-    if (todos.length)
-      window.localStorage.setItem("todos", JSON.stringify(todos));
-  }, [setTodos, todos]);
+function Lane() {
+  const [{ todos, doings, dones }] = useTodo();
   return (
-    <ActionContext.Provider
-      value={{
-        onTodoClick: handleTodoClick,
-        onDoClick: handleDoClick,
-        onDoneClick: handleDoneClick
-      }}
-    >
-      <div className="lane-container">
-        <Content
-          title="Todo"
-          list={todos.filter((todo) => todo.type === "todo")}
-        />
-        <Content
-          title="Doing"
-          list={todos.filter((todo) => todo.type === "doing")}
-        />
-        <Content
-          title="Done"
-          list={todos.filter((todo) => todo.type === "done")}
-        />
-      </div>
-    </ActionContext.Provider>
+    <div className="lane-container">
+      <Content title="Todo" list={todos} />
+      <Content title="Doing" list={doings} />
+      <Content title="Done" list={dones} />
+    </div>
   );
 }
 export default Lane;
