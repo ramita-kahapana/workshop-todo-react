277e6c3ab638dc52a3075fbfcb96c0a8e96513dd
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-07 20:25:15 +0700

    edit-app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from "react";
-import Title from "./components/Title";
-import Input from "./components/Input";
-import Lane from "./components/Lane";
-import { useTodo } from "./contexts/ActionContext";
-import "./styles.css";
+import React, { Suspense, lazy } from "react";
+import { createGlobalStyle } from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import TodoList from "./pages/TodoList";
+// import "./styles.css";
+import "./sass/main.scss"
+
+const TodoList =lazy(()=>import("./pages/TodoList"))
+
+const GlobalStyle = createGlobalStyle`
+hrml,
+body,
+#root {
+  height: 100vh;
+  width: 100vw;
+  font-size: 30px;
+  font-family: "Roboto", sans-serif;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+`;
 
 export default function App() {
-  const [, { handleAddTodo }] = useTodo();
 
   return (
-    <div className="container">
-      <Title>Simple Kanban Board</Title>
-      <Input onSubmit={handleAddTodo} />
-      <Lane />
-    </div>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Suspense fallback="...">
+          <Switch>
+            <Route path="/" exact component={TodoList} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
   );
 }
