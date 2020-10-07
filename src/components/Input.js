92230b113b35a7169148e1d7bf29bdc0397dbec3
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-07 20:25:27 +0700

    edit-input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+
 
 function Input({ onSubmit }) {
   const [value, setValue] = useState("");
@@ -8,14 +11,41 @@ function Input({ onSubmit }) {
     setValue("");
   };
   const handleInputChange = (event) => setValue(event.target.value);
+
+  const BaseInput = styled.input`
+outline: none;
+border: none;
+padding: 0 1rem;
+font-size: 1rem;
+`;
+
+  const InputContainer = styled.form`
+  display: flex;
+  margin-bottom: 1rem`;
+
+  const Input = styled(BaseInput)`
+  diaplay:${props => props.display ? "block" : "none"};
+  padding: 0.5rem 0;
+  border-bottom: 1px solid #ccc;
+  width: 100%;
+  transition: border-bottom 0.3s;
+
+  &:focus{
+    border-bottom: 1px solid #eead00
+  }
+
+  &::placeholder {
+    color: #aaa;
+  }`;
+
   return (
-    <form onSubmit={handleSubmit} className="input-container">
-      <input
+    <InputContainer onSubmit={handleSubmit} className="input-container">
+      <Input
         value={value}
         onChange={handleInputChange}
         placeholder="Create new task"
       />
-    </form>
+    </InputContainer>
   );
 }
 export default Input;
