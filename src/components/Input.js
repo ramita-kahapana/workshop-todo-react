8fef79791cd02fcc20181e055f2e88594dfec9e5
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-19 09:33:48 +0700

    edit-input/component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-
-
 function Input({ onSubmit }) {
   const [value, setValue] = useState("");
   const handleSubmit = (event) => {
@@ -13,30 +11,32 @@ function Input({ onSubmit }) {
   const handleInputChange = (event) => setValue(event.target.value);
 
   const BaseInput = styled.input`
-outline: none;
-border: none;
-padding: 0 1rem;
-font-size: 1rem;
-`;
+    outline: none;
+    border: none;
+    padding: 0 1rem;
+    font-size: 1rem;
+  `;
 
   const InputContainer = styled.form`
-  display: flex;
-  margin-bottom: 1rem`;
+    display: flex;
+    margin-bottom: 1rem;
+  `;
 
   const Input = styled(BaseInput)`
-  diaplay:${props => props.display ? "block" : "none"};
-  padding: 0.5rem 0;
-  border-bottom: 1px solid #ccc;
-  width: 100%;
-  transition: border-bottom 0.3s;
+    diaplay: ${(props) => (props.display ? "block" : "none")};
+    padding: 0.5rem 0;
+    border-bottom: 1px solid #ccc;
+    width: 100%;
+    transition: border-bottom 0.3s;
 
-  &:focus{
-    border-bottom: 1px solid #eead00
-  }
+    &:focus {
+      border-bottom: 1px solid #eead00;
+    }
 
-  &::placeholder {
-    color: #aaa;
-  }`;
+    &::placeholder {
+      color: #aaa;
+    }
+  `;
 
   return (
     <InputContainer onSubmit={handleSubmit} className="input-container">
